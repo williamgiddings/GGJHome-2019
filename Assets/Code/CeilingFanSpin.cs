1fe36ea989829ca65620b67d8b4526cdafa8e10a
Greavesy1899 <xxgreavesxx@gmail.com>
2019-01-26 00:06:00 +0000

    Fixed Fan script

---
+++ Assets/Code/CeilingFanSpin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CeilingFanSpin : MonoBehaviour
+{
+    public float speed = 100f;
+
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0, Space.Self);
+    }
+}
