08def510090751673f7f79974b1a5700443d1bb4
williamgiddings <wgidz99@gmail.com>
2019-01-26 15:38:09 +0000

    Generic3DButton

---
+++ Assets/Code/Generic3DButton.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Generic3DButton : MonoBehaviour
+{
+    public List<UnityEvent> TriggerEvents;
+
+    public void Interact(Transform ply)
+    {
+        foreach (UnityEvent Event in TriggerEvents)
+        {
+            Event.Invoke();
+        }
+    }
+
+}
